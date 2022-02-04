ea282fd98a5c78f799f9b80accd235754236a156
ekiehuang <ekie729@gmail.com>
2022-02-03 22:37:17 -0800

    circuit breaker of hystrix

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/controller/OrderHystrixController.java
@@ -1,8 +1,6 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.service.PaymentHystrixService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +10,7 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+//@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")  //全局的
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -24,9 +23,10 @@ public class OrderHystrixController {
     }
 
     //超时降级演示
-    @HystrixCommand(fallbackMethod = "payment_TimeoutHandler", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")//超过1.5秒就降级自己
-    })
+//    @HystrixCommand(fallbackMethod = "payment_TimeoutHandler", commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")//超过1.5秒就降级自己
+//    })
+//    @HystrixCommand
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
         String result = paymentHystrixService.payment_Timeout(id);
@@ -37,6 +37,11 @@ public class OrderHystrixController {
     //兜底方法，上面方法出问题,我来处理，返回一个出错信息
     public String payment_TimeoutHandler(Integer id) {
         return "我是消费者80,对方支付系统繁忙请10秒后再试。或自己运行出错，请检查自己。";
+    }
+
+    //下面是全局fallback方法
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息，请稍后再试,(┬＿┬)";
     }
 
 }
